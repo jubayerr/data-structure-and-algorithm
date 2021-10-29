93f25a04249b866b9caf3153dd8a8f42383e37be
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-29 23:58:12 +0600

    start data

--- stack.js
+++ stack.js
@@ -1,42 +1,41 @@
 //Last in First out = LIFO
 
-const friends = []
+const friends = [];
 
-friends.push('Jonas')
-friends.push('Martha')
-friends.push('Mikkel')
-friends.push('Bartoze')
+friends.push("Jonas");
+friends.push("Martha");
+friends.push("Mikkel");
+friends.push("Bartoze");
 
 // console.log(friends);
 // const getOne = friends.pop()
 // console.log('Last in First out =>', getOne);
 // console.log(friends);
 
-
 // stack
 
 class Stack {
-    constructor() {
-        this.stack = []
+  constructor() {
+    this.stack = [];
+  }
+  add(item) {
+    this.stack.push(item);
+  }
+  remove() {
+    if (this.stack.length) {
+      return this.stack.pop();
     }
-    add(item) {
-        this.stack.push(item)
-    }
-    remove() {
-        if (this.stack.length) {
-            return this.stack.pop()
-        }
-    }
+  }
 }
 
-const parents = new Stack()
+const parents = new Stack();
 
-parents.add('Hanna Kahnwald')
-parents.add('Ulrich Neilsen')
-parents.add('Kathrina Neilsen')
-parents.add('Regina Tiedemann')
+parents.add("Hanna Kahnwald");
+parents.add("Ulrich Neilsen");
+parents.add("Kathrina Neilsen");
+parents.add("Regina Tiedemann");
 
 console.log(parents.stack);
-const lateIn = parents.remove()
-console.log('LIFO =>', lateIn);
+const lateIn = parents.remove();
+console.log("LIFO =>", lateIn);
 console.log(parents.stack);
